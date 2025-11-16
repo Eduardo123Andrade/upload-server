7d1bfb5bb5c9c7dbbc12d6edbc2b008d58c2ec57
Eduardo Andrade <eduardo.andrade15@hotmail.com>
2025-11-16 15:43:14 -0300

    chore: crate uploads schema and migration

---
+++ src/infra/db/schemas/uploads.ts
@@ -0,0 +1,18 @@
+import { randomUUID } from 'node:crypto'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+export const uploads = pgTable('uploads', {
+  id: text('id')
+    .primaryKey()
+    .$defaultFn(() => randomUUID()),
+  name: text('name').notNull(),
+  remoteKey: text('remote_key').notNull().unique(),
+  remoteUrl: text('remote_url').notNull(),
+  /**
+   * withTimezone is to save the timestamp with timezone information
+   * but use this only if you need operate with this field
+   */
+  //   createdAt: timestamp('created_at', { withTimezone: true })
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updateAt: timestamp('updated_at').notNull().defaultNow(),
+})
